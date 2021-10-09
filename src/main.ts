a831443fb953fdd667218d208c69d6e952545655
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-10-09 11:26:47 -0400

    implement grid and cell types

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,6 @@
-import { Modal, ModeBase } from "./ox";
-
-class Attacking extends ModeBase<Dead | Patrolling> {
-  constructor() {
-    super((onTransition: (nextMode: Dead | Patrolling) => void) => {
-      console.log("update attacking state");
-      onTransition(new Dead());
-    });
-  }
-}
-
-class Dead extends ModeBase<Patrolling> {
-  constructor() {
-    super(() => console.log("update dead state"));
-  }
-}
-
-class Patrolling extends ModeBase<Dead | Pursuing> {
-  constructor() {
-    super((onTransition: (nextMode: Dead | Pursuing) => void) => {
-      console.log("update patrolling state");
-      onTransition(new Pursuing());
-    });
-  }
-}
-
-class Pursuing extends ModeBase<Attacking | Dead | Patrolling> {
-  constructor() {
-    super((onTransition: (nextMode: Attacking | Dead | Pursuing) => void) => {
-      console.log("update pursuing state");
-      onTransition(new Attacking());
-    });
-  }
-}
+import { Grid } from "./ox";
 
 export default () => {
-  const modal = new Modal(new Patrolling());
-  modal.update();
-  modal.update();
-  modal.update();
-  modal.update();
+  const grid = new Grid({ x: 5, y: 5 });
+  console.log(grid.growSelection({ x: 0, y: 0 }));
 };
--- src/ox/grid.ts
+++ src/ox/grid.ts
@@ -1,21 +1,116 @@
 import { Position, Size } from "./math";
 
+// shrink selection
+// filter selection by distance
+// select box
+// select ellipse
+// line-of-sight selection
+
 class Cell<T> {
-  private readonly _grid: Grid<T>;
+  readonly isBottomEdge: boolean;
+  readonly isEdge: boolean;
+  readonly isHorizontalEdge: boolean;
+  readonly isLeftEdge: boolean;
+  readonly isRightEdge: boolean;
+  readonly isTopEdge: boolean;
+  readonly isVerticalEdge: boolean;
   readonly position: Position;
+  readonly selectColumn: () => Cell<T>[];
+  readonly selectNeighbors: () => Cell<T>[];
+  readonly selectRow: () => Cell<T>[];
   constructor(grid: Grid<T>, position: Position) {
-    this._grid = grid;
+    this.isBottomEdge = position.y === grid.size.y;
+    this.isTopEdge = position.y === 0;
+    this.isLeftEdge = position.x === 0;
+    this.isRightEdge = position.x === grid.size.x;
+    this.isHorizontalEdge = this.isLeftEdge || this.isRightEdge;
+    this.isVerticalEdge = this.isBottomEdge || this.isTopEdge;
+    this.isEdge = this.isHorizontalEdge || this.isVerticalEdge;
     this.position = position;
+    this.selectColumn = () => grid.selectColumns(position);
+    this.selectNeighbors = () =>
+      grid.select(
+        undefined,
+        { x: position.x - 1, y: position.y - 1 },
+        { x: position.x - 1, y: position.y },
+        { x: position.x - 1, y: position.y + 1 },
+        { x: position.x, y: position.y - 1 },
+        { x: position.x, y: position.y + 1 },
+        { x: position.x + 1, y: position.y - 1 },
+        { x: position.x + 1, y: position.y },
+        { x: position.x + 1, y: position.y + 1 }
+      );
+    this.selectRow = () => grid.selectRows(position);
   }
 }
 
-class Grid<T> {
+export class Grid<T> {
   private readonly _cells: Cell<T>[] = [];
+  readonly growSelection: (...origins: Position[]) => Cell<T>[];
+  readonly size: Size;
+  readonly select: (
+    onFilter?: (cell: Cell<T>, index: number, origins: Position[]) => boolean,
+    ...origins: Position[]
+  ) => Cell<T>[];
+  readonly selectAll: () => Cell<T>[];
+  readonly selectBottomEdge: () => Cell<T>[];
+  readonly selectColumns: (...origins: Position[]) => Cell<T>[];
+  readonly selectEdges: () => Cell<T>[];
+  readonly selectHorizontalEdges: () => Cell<T>[];
+  readonly selectLeftEdge: () => Cell<T>[];
+  readonly selectRightEdge: () => Cell<T>[];
+  readonly selectRows: (...origins: Position[]) => Cell<T>[];
+  readonly selectTopEdge: () => Cell<T>[];
+  readonly selectVerticalEdges: () => Cell<T>[];
   constructor(size: Size) {
-    for (let x = 0; x < size.x; x++) {
-      for (let y = 0; y < size.y; y++) {
+    this.size = size;
+    for (let x = 0; x < this.size.x; x++) {
+      for (let y = 0; y < this.size.y; y++) {
         this._cells.push(new Cell<T>(this, { x, y }));
       }
     }
+    this.growSelection = (...origins: Position[]): Cell<T>[] => [
+      ...new Set(
+        origins.flatMap((origin) => {
+          const cell = this.select(undefined, origin)[0];
+          return cell ? [cell, ...cell.selectNeighbors()] : [];
+        })
+      ),
+    ];
+    this.select = (
+      onFilter?: (cell: Cell<T>, index: number, origins: Position[]) => boolean,
+      ...origins: Position[]
+    ): Cell<T>[] =>
+      this._cells.filter((cell, index) =>
+        onFilter
+          ? onFilter(cell, index, origins)
+          : origins.some(
+              (origin) =>
+                origin.x === cell.position.x && origin.y === cell.position.y
+            )
+      );
+    this.selectAll = () => this._cells;
+    this.selectBottomEdge = () =>
+      this._cells.filter((cell) => cell.isBottomEdge);
+    this.selectColumns = (...origins: Position[]): Cell<T>[] =>
+      this.select(
+        (cell, _index, origins) =>
+          origins.some((origin) => cell.position.x === origin.x),
+        ...origins
+      );
+    this.selectEdges = () => this._cells.filter((cell) => cell.isEdge);
+    this.selectHorizontalEdges = () =>
+      this._cells.filter((cell) => cell.isHorizontalEdge);
+    this.selectLeftEdge = () => this._cells.filter((cell) => cell.isLeftEdge);
+    this.selectRightEdge = () => this._cells.filter((cell) => cell.isRightEdge);
+    this.selectRows = (...origins: Position[]): Cell<T>[] =>
+      this.select(
+        (cell, _index, origins) =>
+          origins.some((origin) => cell.position.y === origin.y),
+        ...origins
+      );
+    this.selectTopEdge = () => this._cells.filter((cell) => cell.isTopEdge);
+    this.selectVerticalEdges = () =>
+      this._cells.filter((cell) => cell.isVerticalEdge);
   }
 }
