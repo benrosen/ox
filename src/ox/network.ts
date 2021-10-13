3cc52d83c244713ca719c4d0619afde988b3810c
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-10-12 20:44:29 -0400

    make client not generic

--- src/ox/network.ts
+++ src/ox/network.ts
@@ -9,29 +9,29 @@ type Server = {
   url: Url;
 };
 
-export class Client<T> extends Suite<Data<T>> {
+export class Client extends Suite<Data<string>> {
   constructor(server: Server) {
     super(...Object.values(ClientEvent));
     const websocket = new WebSocket(server.url, server.protocols);
     [
       {
         event: WebsocketEvent.OnClose,
-        listener: (event: Data<T>) =>
+        listener: (event: Data<string>) =>
           this.events.onClose.publish({ data: event }),
       },
       {
         event: WebsocketEvent.OnError,
-        listener: (event: Data<T>) =>
+        listener: (event: Data<string>) =>
           this.events.onError.publish({ data: event }),
       },
       {
         event: WebsocketEvent.OnMessage,
-        listener: (event: Data<T>) =>
+        listener: (event: Data<string>) =>
           this.events.onMessage.publish({ data: event }),
       },
       {
         event: WebsocketEvent.OnOpen,
-        listener: (event: Data<T>) =>
+        listener: (event: Data<string>) =>
           this.events.onOpen.publish({ data: event }),
       },
     ].forEach(({ event, listener }) =>
