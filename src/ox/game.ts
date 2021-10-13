4dee20a362bd610a9b01ca688c13719795ffa619
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-10-13 17:18:26 -0400

    add scene manager to game

--- src/ox/game.ts
+++ src/ox/game.ts
@@ -1,12 +1,23 @@
 import { Clock, ClockConfig } from "./clocks";
 
+import { Modal } from "./modes";
 import { Roster } from "./agents";
+import { Scene } from "./scenes";
 
 export class Game {
   readonly clock: Clock;
   readonly roster: Roster;
-  constructor({ clock: clockConfig }: { clock: ClockConfig }) {
+  readonly sceneManager: Modal;
+  constructor({
+    clockConfig,
+    initialScene,
+  }: {
+    clockConfig: ClockConfig;
+    initialScene: Scene;
+  }) {
     this.clock = new Clock(clockConfig);
     this.roster = new Roster();
+    this.sceneManager = new Modal(initialScene);
+    this.clock.events.onChange.subscribe(() => this.sceneManager.update());
   }
 }
