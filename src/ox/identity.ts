349023510b95e2d8a5689329d4d09d1f43f2bf1c
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-10-12 20:42:05 -0400

    add function to create uuid identifier

--- src/ox/identity.ts
+++ src/ox/identity.ts
@@ -3,3 +3,12 @@ export interface Identifiable {
 }
 
 export type Identifier = string;
+
+export const createIdentifier = (): Identifier =>
+  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    (character: string): string => {
+      const random = (Math.random() * 16) | 0;
+      return (character === "x" ? random : (random & 0x3) | 0x8).toString(16);
+    }
+  );
