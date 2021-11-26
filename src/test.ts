e43245c4d73ccb12b81ce879d7fd5e882d9c3add
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-11-25 19:13:39 -0500

    test edge-related properties of Cell

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,28 @@
-import { Cell, Grid, ProtectedVariable, PublicTopic } from "./main";
+import {
+  Cell,
+  Grid,
+  ProtectedVariable,
+  PublicTopic,
+  Token,
+  getCellFromGridByCoordinates,
+} from "./main";
 
 describe("ox", () => {
   describe("`Cell`", () => {
-    const grid = new Grid([3, 3], new ProtectedVariable(new PublicTopic()));
+    const setTokens = new PublicTopic<Set<Token>>();
+    const tokens = new ProtectedVariable<Set<Token>>(setTokens);
+    setTokens.publish(new Set());
+    const grid = new Grid([3, 3], tokens);
+
     describe("`.column`", () =>
-      [
-        [0, 0],
-        [1, 1],
-        [2, 2],
-      ]
-        .map(
-          (coordinates) =>
-            Array.from(grid.cells).find(
-              (cell) => cell.coordinates === coordinates
-            )!
-        )
+      (
+        [
+          [0, 0],
+          [1, 1],
+          [2, 2],
+        ] as [number, number][]
+      )
+        .map((coordinates) => getCellFromGridByCoordinates(coordinates, grid))
         .forEach((cell) => {
           it("returns a collection whose size equals the height of the cell's parent grid.", () =>
             expect(cell.column.size).toStrictEqual(cell.grid.dimensions[1]));
@@ -30,17 +38,17 @@ describe("ox", () => {
         }));
 
     describe("`.isOnBottomEdge`", () =>
-      [
-        [[0, 0], false],
-        [[1, 1], false],
-        [[2, 2], true],
-      ]
+      (
+        [
+          [[0, 0], false],
+          [[1, 1], false],
+          [[2, 2], true],
+        ] as [[number, number], boolean][]
+      )
         .map(
           ([coordinates, isCellOnBottomEdge]) =>
             [
-              Array.from(grid.cells).find(
-                (cell) => cell.coordinates === coordinates
-              )!,
+              getCellFromGridByCoordinates(coordinates, grid),
               isCellOnBottomEdge,
             ] as [Cell, boolean]
         )
@@ -49,41 +57,139 @@ describe("ox", () => {
             expect(cell.isOnBottomEdge).toStrictEqual(isCellOnBottomEdge));
         }));
 
-    // describe("`.isOnEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the top, right, bottom, or left edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnEdge`", () =>
+      (
+        [
+          [[0, 0], true],
+          [[0, 1], true],
+          [[1, 1], false],
+          [[2, 0], true],
+          [[2, 1], true],
+          [[2, 2], true],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnEdge]) =>
+            [getCellFromGridByCoordinates(coordinates, grid), isCellOnEdge] as [
+              Cell,
+              boolean
+            ]
+        )
+        .forEach(([cell, isCellOnEdge]) => {
+          it("returns `true` if this cell is positioned on the top, right, bottom, or left edge of its parent grid.", () => {
+            expect(cell.isOnEdge).toStrictEqual(isCellOnEdge);
+          });
+        }));
 
-    // describe("`.isOnHorizontalEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the top or bottom edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnHorizontalEdge`", () =>
+      (
+        [
+          [[0, 0], true],
+          [[0, 1], false],
+          [[1, 1], false],
+          [[2, 0], true],
+          [[2, 1], false],
+          [[2, 2], true],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnHorizontalEdge]) =>
+            [
+              getCellFromGridByCoordinates(coordinates, grid),
+              isCellOnHorizontalEdge,
+            ] as [Cell, boolean]
+        )
+        .forEach(([cell, isCellOnHorizontalEdge]) =>
+          it("returns `true` if this cell is positioned on the top or bottom edge of its parent grid.", () => {
+            expect(cell.isOnHorizontalEdge).toStrictEqual(
+              isCellOnHorizontalEdge
+            );
+          })
+        ));
 
-    // describe("`.isOnLeftEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the left edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnLeftEdge`", () =>
+      (
+        [
+          [[0, 0], true],
+          [[1, 1], false],
+          [[2, 2], false],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnLeftEdge]) =>
+            [
+              getCellFromGridByCoordinates(coordinates, grid),
+              isCellOnLeftEdge,
+            ] as [Cell, boolean]
+        )
+        .forEach(([cell, isCellOnLeftEdge]) => {
+          it("returns `true` if this cell is positioned on the left edge of its parent grid.", () =>
+            expect(cell.isOnLeftEdge).toStrictEqual(isCellOnLeftEdge));
+        }));
 
-    // describe("`.isOnRightEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the right edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnRightEdge`", () =>
+      (
+        [
+          [[0, 0], false],
+          [[1, 1], false],
+          [[2, 2], true],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnRightEdge]) =>
+            [
+              getCellFromGridByCoordinates(coordinates, grid),
+              isCellOnRightEdge,
+            ] as [Cell, boolean]
+        )
+        .forEach(([cell, isCellOnRightEdge]) => {
+          it("returns `true` if this cell is positioned on the right edge of its parent grid.", () =>
+            expect(cell.isOnRightEdge).toStrictEqual(isCellOnRightEdge));
+        }));
 
-    // describe("`.isOnTopEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the top edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnTopEdge`", () =>
+      (
+        [
+          [[0, 0], true],
+          [[1, 1], false],
+          [[2, 2], false],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnTopEdge]) =>
+            [
+              getCellFromGridByCoordinates(coordinates, grid),
+              isCellOnTopEdge,
+            ] as [Cell, boolean]
+        )
+        .forEach(([cell, isCellOnTopEdge]) => {
+          it("returns `true` if this cell is positioned on the bottom edge of its parent grid.", () =>
+            expect(cell.isOnTopEdge).toStrictEqual(isCellOnTopEdge));
+        }));
 
-    // describe("`.isOnVerticalEdge`", () => {
-    //   it("returns `true` if this cell is positioned on the left or right edge of its parent grid.", () => {
-    //     //
-    //   });
-    // });
+    describe("`.isOnVerticalEdge`", () =>
+      (
+        [
+          [[0, 0], true],
+          [[0, 1], true],
+          [[1, 1], false],
+          [[2, 0], true],
+          [[2, 1], true],
+          [[2, 2], true],
+        ] as [[number, number], boolean][]
+      )
+        .map(
+          ([coordinates, isCellOnVerticalEdge]) =>
+            [
+              getCellFromGridByCoordinates(coordinates, grid),
+              isCellOnVerticalEdge,
+            ] as [Cell, boolean]
+        )
+        .forEach(([cell, isCellOnVerticalEdge]) =>
+          it("returns `true` if this cell is positioned on the top or bottom edge of its parent grid.", () => {
+            expect(cell.isOnVerticalEdge).toStrictEqual(isCellOnVerticalEdge);
+          })
+        ));
 
     // describe("`.negativeDiagonal`", () => {
     //   it("returns the cells that share an `x` coordinate with this cell, including this cell.", () => {
